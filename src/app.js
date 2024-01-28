16845c2fdd335145ad83c54cdfe1336512818b30
Abhishek Vashishth <abhivashishtha00@gmailcom>
2024-01-28 23:52:53 +0530

    app configuration

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from "express";
-
+import cors from "cors";
+import cookieParser from "cookie-parser"
 const app = express();
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"})) // as in url sometimes data become not usable jo we used this middleware
+app.use(express.static("public"))
+app.use(cookieParser());
 
 export {app};
